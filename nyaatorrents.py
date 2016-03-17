c57b34e42db685f2d518b25af29f0f8a3570e503
Yukariin <Yukariin@users.noreply.github.com>
2016-03-17 23:13:09 +0300

    Fix version and use https

--- nyaatorrents.py
+++ nyaatorrents.py
@@ -1,17 +1,17 @@
-# VERSION: 2.26
-# AUTHORS: Yukarin (yukariin@yandex.ru)
+#VERSION: 2.27
+#AUTHORS: Yukarin (yukariin@yandex.ru)
 
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
 #
-# * Redistributions of source code must retain the above copyright notice,
-# this list of conditions and the following disclaimer.
-# * Redistributions in binary form must reproduce the above copyright
-# notice, this list of conditions and the following disclaimer in the
-# documentation and/or other materials provided with the distribution.
-# * Neither the name of the author nor the names of its contributors may be
-# used to endorse or promote products derived from this software without
-# specific prior written permission.
+#    * Redistributions of source code must retain the above copyright notice,
+#      this list of conditions and the following disclaimer.
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#    * Neither the name of the author nor the names of its contributors may be
+#      used to endorse or promote products derived from this software without
+#      specific prior written permission.
 #
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -38,7 +38,7 @@ from helpers import retrieve_url, download_file
 
 
 class nyaatorrents(object):
-    url = 'http://www.nyaa.se'
+    url = 'https://www.nyaa.se'
     name = 'Nyaatorrents'
     supported_categories = {'all': '0_0',
                             'anime': '1_0',
